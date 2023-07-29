aa1f1c21ac3f24340a0d920e2c1a7059f415ad8c
amitsingh-24 <imamitsingh958@gmail.com>
2023-07-29 16:35:57 +0530

    Updated Site

--- mail/contact.js
+++ mail/contact.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
@@ -14,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php", // Corrected the URL
+                url: "contact.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -25,14 +26,18 @@ $(function () {
                 cache: false,
                 success: function () {
                     $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
-                    $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success').append('</div>');
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                            .append('</div>');
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
                     $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
